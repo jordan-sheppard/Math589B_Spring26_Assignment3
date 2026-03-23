224148722db20226f19c68f81fd3fa7af709647a
Jordan Sheppard <jg.sheppard808@gmail.com>
2026-03-23 16:20:34 -0700

    Implemented direct method for solve_continuous_are()

--- src/python/student.py
+++ src/python/student.py
@@ -11,3 +11,38 @@
 #
 # 1. simulate_closed_loop
 # 2. simulate_open_loop
+
+import numpy as np
+
+def solve_continous_are(A: np.ndarray, B: np.ndarray, Q: np.ndarray, R: np.ndarray) -> np.ndarray:
+    """
+    Solve the continuous Algebraic Ricatti Equation using the Direct method.
+
+    Args:
+        A, B : System matrices for the state dynamics
+        Q, R : Weight matrices for the state and input in the LQR cost function
+
+    Returns:
+        P: Solution to the ARE
+    """
+    # Construct Hamilton matrix
+    H = np.block(
+        [
+            [A , -B @ np.linalg.inv(R) @ B.T],
+            [-Q, -A.T],
+        ]
+    )
+
+    # Find stable eigenvectors of H
+    eigenvals, eigenvecs = np.linalg.eig(A)
+    stable_indices = np.real(eigenvals) < 0
+    stable_eigenvecs = eigenvecs[:,stable_indices]
+
+    # Partition
+    n = A.shape[0]
+    V_s1 = stable_eigenvecs[:n, :]
+    V_s2 = stable_eigenvecs[n:, :]
+
+    # Solve for P
+    P = V_s2 @ np.linalg.inv(V_s1)
+    return (P + P.T) / 2            # Enforce symmetry numerically
